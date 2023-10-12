e5dbd5870a73619b22d14b3b913369321f33a12a
K.D.G <143414166+gyun97@users.noreply.github.com>
2023-10-13 03:27:48 +0900

    Add files via upload

---
+++ 스프링 입문/09 회원 도메인과 리포지토리 테스트/study/hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findbyID(Long id);
+    Optional<Member> findbyName(String name);
+    List<Member> findAll();
+
+}
---
+++ 스프링 입문/09 회원 도메인과 리포지토리 테스트/study/hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,48 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); // Member 객체를 저장하는 Map 컬렉션
+    private static long sequence = 0L; // sequence: 회원의 고유한 id를 생성하기 위한 시퀀스 번호
+
+    @Override
+    public Member save(Member member) {  // 새로운 회원 정보를 저장하고 반환
+
+        member.setId(++sequence); // 새 멤버 저장될 때마다 이전보다 1 늘어난 id 생성
+        store.put(member.getId(), member);  // put(): map에 키-값 쌍 추가
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findbyID(Long id) { // 특정 id를 가진 회원을 조회
+
+        return Optional.ofNullable(store.get(id));  // Optional.ofNullable(): () 안의 값이 Null이어도 감쌀 수 있다
+    }                                              // 해당 id의 회원이 없으면 빈 Optional을 반환
+
+    @Override
+    public Optional<Member> findbyName(String name) {  // 회원의 이름(name)으로 회원을 조회
+
+        // values()로 store의 멤버 객체들을 컬렉션화하고 stream()으로 스트림 객체 생성하여 하나씩 순회 처리
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name)) // filter(lambda)로 멤버 이름이 파라미터 name과의 일치 확인
+                .findAny();  // 조건에 맞는 요소를 찾아서 반환, 없으면 빈 값(Optional)을 반환
+    }
+
+    @Override
+    public List<Member> findAll() { //저장된 모든 회원 목록을 반환
+        return new ArrayList<>(store.values()); // store에 저장된 모든 Member 객체를 리스트로 변환하여 반환
+    }
+
+
+    public void clearStore() {
+        store.clear(); // List의 모든 객체 삭제
+    }
+
+
+}
+
+
